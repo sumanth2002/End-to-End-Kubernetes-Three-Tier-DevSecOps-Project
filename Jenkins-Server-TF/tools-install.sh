04eaa3b0143daa3193f8d8d44aae7ebdc7a7dd76
Sumanth <69471771+sumanth2002@users.noreply.github.com>
2024-08-17 10:01:37 +0530

    fixed error in line 69 tools-install.sh

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -66,7 +66,7 @@ sudo apt install terraform -y
 #!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+echo "deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt update
 sudo apt install trivy -y
 
